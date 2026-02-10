836f12d19bcfffe2e73ab1f969141330d53e0d9a
MO <amirtamer31@gmail.com>
2026-02-10 14:28:06 +0400

    fixed positioning of text

--- client/src/components/TextSection.jsx
+++ client/src/components/TextSection.jsx
@@ -1,8 +1,8 @@
 import Box from "@mui/material/Box";
 export default function TextSection() {
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: 672, height: 274, flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', maxwidth: 672, height: 120, justifyContent: 'space-between' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: 672, height: 274, flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxwidth: 672, height: 120, justifyContent: 'space-between' }}>
                 <h1 style={{
                     fontFamily: `"Helvetica Neue", Helvetica, Arial, sans-serif`,
                     fontSize: "48px",
@@ -34,9 +34,9 @@ export default function TextSection() {
                     Transaction-Driven Insight.</h1>
             </Box>
             <Box>
-                <p style={{ fontSize: '16px', fontFamily: `"Helvetica Neue", Helvetica, Arial, sans-serif`, color: 'grey' }}>Transaction-level analysis powered by RFM modeling and unsupervised learning to uncover meaningful customer segments and actionable behavioral patterns.</p>
+                <p style={{ fontSize: '16px', fontFamily: `"Helvetica Neue", Helvetica, Arial, sans-serif`, color: 'grey', textAlign: 'center' }}>Transaction-level analysis powered by RFM modeling and unsupervised learning to uncover meaningful customer segments and actionable behavioral patterns.</p>
             </Box>
-        </Box>
+        </Box >
     )
 
 
